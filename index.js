2234d141c2051202e7f4be17b3411a61452bbc0f
Erika Cannon <cannone.rva@gmail.com>
2022-05-23 18:51:54 -0400

    index update

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const db = mysql.createConnection(
 );
 
 // Start server after DB connection
-<<<<<<< HEAD
 db.connect((err) => {
   if (err) throw err;
 
@@ -235,78 +234,3 @@ function exit() {
   console.log("GOODBYE!");
   return;
 }
-=======
-db.connect(err => {
-    if (err) throw err;
-
-    console.log('Database connected.');
-    
-  promptUser()
-  });
-
-   //add inquirer prompts
-   function promptUser() {
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'directory',
-          message: 'What would you like to do?',
-          choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add department',
-            'Add role',
-            'Add employee',
-            'Update employee role',
-            'Exit',
-          ],
-        },
-      ])
-      .then((response) => {
-        if (response.directory === 'View all departments') {
-          viewDeps();
-        } else if (response.directory === 'View all roles') {
-          viewRoles();
-        } else if (response.directory === 'View all employees') {
-          viewEmps();
-        } else if (response.directory === 'Add department') {
-          addDep();
-        } else if (response.directory === 'Add role') {
-          addRole();
-        } else if (response.directory === 'Add employee') {
-          addEmp();
-        } else if (response.directory === 'Update employee role') {
-          updateRole();
-        } else if (response.directory === 'Exit') {
-          exit();
-        }
-      });
-   }
-//inquirer prompt functions
-   //view all departments
-function viewDeps(){
-};
-//view all roles
-function viewRoles(){
-};
-//veiw all employee
-function  viewEmps(){
-};
-//add a department
-function addDep(){
-};
-// add a role
-function addRole(){
-};
-// add an employee
-function  addEmp(){
-};
-//update an existing role
-function updateRole(){
-};
-//stop inquirer
-function exit(){
-};
->>>>>>> b57f04d70c38220de6d3b65a48b278e60196a128
