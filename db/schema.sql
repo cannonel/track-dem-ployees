fa5b72c180fc56db9a325ee03184913a4cf3b70e
Erika Cannon <cannone.rva@gmail.com>
2022-05-23 18:47:29 -0400

    add inquirer prompts, updated index and db

--- db/schema.sql
+++ db/schema.sql
@@ -2,27 +2,26 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
 
-
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+id INTEGER AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INTEGER AUTO_INCREMENT, PRIMARY KEY, NOT NULL,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER ,
+  salary DECIMAL(30) NOT NULL,
+  department_id INTEGER,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY, NOT NULL,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER
-   CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-   CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  manager_id INTEGER,
+  CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+--   CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,10 +1,236 @@
-const db = require('./db/connection');
+const { connect } = require("http2");
+const inquirer = require("inquirer");
+const { Connection } = require("mysql2");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3305;
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: "employee_database",
+  },
+  console.log(`Connected on ${PORT}`)
+);
 
 // Start server after DB connection
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+db.connect((err) => {
+  if (err) throw err;
+
+  console.log("Database connected.");
+
+  //AREA TO ADD INQUIRER PROMPTS AND CONSOLE LOG INQUIRERE SPLASH SCREEN
+  console.log("WELCOME");
+  console.log("TO");
+  console.log("EMPLOYEE");
+  console.log("TRACKER");
+  promptUser();
+});
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "directory",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add department",
+          "Add role",
+          "Add employee",
+          "Update employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      if (response.directory === "View all departments") {
+        viewDeps();
+      } else if (response.directory === "View all roles") {
+        viewRoles();
+      } else if (response.directory === "View all employees") {
+        viewEmps();
+      } else if (response.directory === "Add department") {
+        addDep();
+      } else if (response.directory === "Add role") {
+        addRole();
+      } else if (response.directory === "Add employee") {
+        addEmp();
+      } else if (response.directory === "Update employee role") {
+        updateRole();
+      } else if (response.directory === "Exit") {
+        exit();
+      }
     });
+}
+
+function viewDeps() {
+  let sql = `SELECT * FROM department`;
+  db.query(sql, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    console.log(`
+    =========================`);
+    promptUser();
   });
+}
+
+function viewRoles() {
+  let sql = `SELECT * FROM roles`;
+  db.query(sql, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    console.log(`
+    =========================`);
+    promptUser();
+  });
+}
+
+function viewEmps() {
+  let sql = `SELECT * FROM employee`;
+  db.query(sql, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    console.log(`
+    =========================`);
+    promptUser();
+  });
+}
+
+async function addDep() {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "depName",
+        message: "What is the name of the new department?",
+      },
+    ])
+    .then((answer) => {
+      db.query(`INSERT INTO department SET ?`, {
+        department_name: answer.depName,
+      });
+    });
+  console.log(`Database updated
+      =============================`);
+  promptUser();
+}
+
+async function addRole() {
+  // name, salary, department
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "Enter the name of the new role:",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "Enter the salary associated with this role: (no commas)",
+      },
+      {
+        type: "input",
+        name: "roleDep",
+        message: "Please enter the department ID for this role: ",
+      },
+    ])
+    .then((answer) => {
+      if (answer === "Other") {
+        addDep();
+      }
+      const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+      const params = [answer.roleName, answer.roleSalary, answer.roleDep];
+      db.query(sql, params, (err) => {
+        if (err) throw err;
+        console.log(`Database updated
+      =============================`);
+        promptUser();
+      });
+    });
+}
+
+async function addEmp() {
+  // first, last, role, manager
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Enter the employee first name:",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Enter the employee last name:",
+      },
+      {
+        type: "input",
+        name: "empRole",
+        message: "What is the role ID associated with this employee?",
+      },
+      {
+        type: "input",
+        name: "empMgr",
+        message: "Please enter the manager ID associated for this employee: ",
+      },
+    ])
+    .then((answer) => {
+      if (answer.empRole === "Other") {
+        addRole();
+      }
+      const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+      const params = [
+        answer.firstName,
+        answer.lastName,
+        answer.empRole,
+        answer.empMgr,
+      ];
+      db.query(sql, params, (err) => {
+        if (err) throw err;
+        console.log(`Database updated
+      =============================`);
+        promptUser();
+      });
+    });
+}
+
+async function updateRole() {
+  // update role
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "empID",
+        message:
+          "Enter the ID number of the employee you would like to update:",
+      },
+      {
+        type: "input",
+        name: "roleID",
+        message: "Enter the ID of the new role for this employee: ",
+      },
+    ])
+    .then((answer) => {
+      const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      const params = [answer.roleID, answer.empID];
+      db.query(sql, params, (err) => {
+        if (err) throw err;
+        console.log(`Database updated
+      =============================`);
+        promptUser();
+      });
+    });
+}
+
+function exit() {
+  console.log("GOODBYE!");
+  return;
+}
